913272d6403ff53c2b9c3090568f3180eae85e87
bakhr0mkhan <softdeveloperuz@gmail.com>
2021-10-23 11:37:33 +0500

    Initial

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>APP</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
